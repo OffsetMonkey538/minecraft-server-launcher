18f66f5ad54408e6fbab78260c06b4d31c045c00
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-07-15 16:08:19 +0300

    Add all the code

--- src/main/java/top/offsetmonkey538/Main.java
+++ src/main/java/top/offsetmonkey538/Main.java
@@ -1,7 +1,57 @@
 package top.offsetmonkey538;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Loading args files...");
+        final String preLaunchJarArgs = loadArgs("pre-launch.txt");
+        final String actualJarArgs = loadArgs("actual-jar.txt");
+
+
+        runJarFile(preLaunchJarArgs);
+        runJarFile(actualJarArgs);
+    }
+
+
+    @NotNull
+    private static String loadArgs(final String fileName) {
+        final Path filePath = Path.of(fileName);
+
+        try {
+            return Files.readString(filePath);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load args for file " + fileName, e);
+        }
+    }
+
+    private static void runJarFile(@NotNull String argsFile) {
+        final List<String> args = new ArrayList<>();
+        args.add("java");
+        args.addAll(Arrays.stream(argsFile.split(" ")).map(String::trim).toList());
+
+        System.out.println("Starting process with args: " + args);
+
+        try {
+            final ProcessBuilder processBuilder = new ProcessBuilder(args);
+            processBuilder.redirectErrorStream(true);
+            processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+            final Process process = processBuilder.start();
+
+            process.waitFor();
+            final int exitCode = process.exitValue();
+            if (exitCode != 0) {
+                throw new IOException("Failed to execute jar!");
+            }
+        } catch (final IOException | InterruptedException e) {
+            throw new RuntimeException("Failed to execute jar!", e);
+        }
     }
 }
